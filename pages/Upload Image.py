76cc933ae2ae6a3764ad9bf5b8fa8cecd358bee5
Lamees-F <106197989+Lamees-F@users.noreply.github.com>
2025-01-06 18:29:22 +0300

    add logic

--- pages/Upload Image.py
+++ pages/Upload Image.py
@@ -43,21 +43,22 @@ with col3:
 
 # Process the image to detect chess pieces
 def process_image(imagePath):
-    results = model.predict(source=imagePath, conf=st.session_state.conf_threshold)
+    if not 'chessboard' in st.session_state:
+        results = model.predict(source=imagePath, conf=st.session_state.conf_threshold)
 
-    if results:
-        boxes = results[0].boxes.xyxy.cpu().numpy()
-        predicted_classes = results[0].boxes.cls
-        class_names = model.names
-        predicted_class_names = [class_names[int(cls)] for cls in predicted_classes]
-
-        detection_vis = results[0].plot()
-        det_out.image(detection_vis, channels="BGR", use_container_width=True)
-        st.session_state.previous_board_status = order_detections(boxes, predicted_class_names)
-        # Update the board based on detection results
-        st.session_state.chessboard = update_board_and_extract_pawns(st.session_state.previous_board_status)
-        print(st.session_state.chessboard)
-        board_svg_placeholder.markdown(update_board_display(st.session_state.chessboard), unsafe_allow_html=True)
+        if results:
+            boxes = results[0].boxes.xyxy.cpu().numpy()
+            predicted_classes = results[0].boxes.cls
+            class_names = model.names
+            predicted_class_names = [class_names[int(cls)] for cls in predicted_classes]
+        
+            detection_vis = results[0].plot()
+            det_out.image(detection_vis, channels="BGR", use_container_width=True)
+            st.session_state.previous_board_status = order_detections(boxes, predicted_class_names)
+            # Update the board based on detection results
+            st.session_state.chessboard = update_board_and_extract_pawns(st.session_state.previous_board_status)
+            print(st.session_state.chessboard)
+    board_svg_placeholder.markdown(update_board_display(st.session_state.chessboard), unsafe_allow_html=True)
 
 # Helper function to update the chessboard based on detected pieces
 def update_board_and_extract_pawns(board_status):
@@ -106,18 +107,47 @@ if uploaded_file:
 
 pawn_choice = st.radio("Select Pawn Color", ("Black Pawns", "White Pawns"),None)
 available_pieces = ["Knight", "Rook", "Bishop", "Queen", "King"]
+piece_map = {
+    "Knight": "N",  # Knight -> 'N'
+    "Rook": "R",    # Rook -> 'R'
+    "Bishop": "B",  # Bishop -> 'B'
+    "Queen": "Q",   # Queen -> 'Q'
+    "King": "K"     # King -> 'K'
+}
 
 if pawn_choice == "Black Pawns":
     if black_pawn_positions:  
-        st.selectbox("Black Pawn Positions:", options=black_pawn_positions)
-        piece_choice = st.radio("Choose a piece to place:", available_pieces,None)
+        st.session_state.selected_position = st.selectbox("Black Pawn Positions:", options=black_pawn_positions)
+        st.session_state.piece_choice = st.radio("Choose a piece to place:", available_pieces, None)
+
+        if st.session_statepiece_choice:
+            piece_symbol = piece_map.get(st.session_state.piece_choice)  
+            piece = chess.Piece.from_symbol(piece_symbol.lower())  
+            square = chess.parse_square(st.session_state.selected_position)  
+
+            st.session_state.chessboard.set_piece_at(square, piece)
+            st.session_state.chessboard = st.session_state.chessboard  
+
+            # Update the displayed board
+            board_svg_placeholder.markdown(update_board_display(st.session_state.chessboard), unsafe_allow_html=True)
 
     else:
-        st.write("Upload image to detect pieces.")
+        st.write("Upload an image to detect pieces.")
 else:
     if white_pawn_positions: 
-        st.selectbox("White Pawn Positions:", options=white_pawn_positions)
-        piece_choice = st.radio("Choose a piece to place:", available_pieces,None)
-    else:
-        st.write("Upload image to detect pieces.")
+        st.session_state.selected_position = st.selectbox("White Pawn Positions:", options=white_pawn_positions)
+        st.session_state.piece_choice = st.radio("Choose a piece to place:", available_pieces, None)
 
+        if st.session_state.piece_choice:
+            piece_symbol = piece_map.get(st.session_state.piece_choice)  
+            piece = chess.Piece.from_symbol(piece_symbol) 
+            square = chess.parse_square(st.session_state.selected_position)
+
+            st.session_state.chessboard.set_piece_at(square, piece)
+            st.session_state.chessboard = st.session_state.chessboard  
+
+            # Update the displayed board
+            board_svg_placeholder.markdown(update_board_display(st.session_state.chessboard), unsafe_allow_html=True)
+
+    else:
+        st.write("Upload an image to detect pieces.")
